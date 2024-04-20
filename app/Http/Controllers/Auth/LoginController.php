4f62063202ee2d3e3474bf668eb1e919a9548eb1
Mahnev_SA <still-1994@mail.ru>
2024-04-20 20:01:20 +0200

    verify

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
@@ -14,6 +15,20 @@ class LoginController extends Controller
 
     public function verify(Request $request)
     {
-        dd($request);
+        $this->validate($request, [
+            'name' => 'required',
+            'phone' => 'required|min:16', // Убедитесь, что формат номера соответствует вашим требованиям
+        ]);
+
+        $user = User::where('name', $request->name)
+            ->where('phone', $request->phone)
+            ->first();
+
+        if ($user) {
+            return response()->json(['message' => 'User verified', 'verified' => true]);
+        } else {
+            return response()->json(['message' => 'User not found', 'verified' => false], 404);
+        }
     }
+
 }
