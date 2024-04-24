bfbe98fc4fe228e246664795f0129397e8b87f1e
Makhnev_SA <ragnoboy123@gmail.com>
2024-04-24 16:00:37 +0200

    Landing

--- resources/js/components/LandingBody3.vue
+++ resources/js/components/LandingBody3.vue
@@ -5,28 +5,32 @@
                 <h1>Flow Expert AI</h1>
                 <p>— это цифровая платформа для сегмента B2B, которая позволяет:</p>
             </div>
-            <div class="grid">
-                <div class="col-6">
+            <div class="flex align-items-stretch flex-column lg:flex-row">
+                <div class="flex mt-3 lg:col-6">
                     <i class="pi pi-check-circle feature-icon"></i>
                     <span>Формировать запросы и получать коммерческие предложения от неограниченного количества дистрибьюторов как национальных, так и локальных;</span>
                 </div>
-                <div class="col-6">
+                <div class="flex mt-3 lg:col-6">
                     <i class="pi pi-link feature-icon"></i>
                     <span>Обеспечивать кросс-платформенное связывание номенклатур лекарственных препаратов с целью создания единой структурированной базы;</span>
                 </div>
-                <div class="col-6">
+            </div>
+            <div class="flex align-items-stretch flex-column lg:flex-row">
+                <div class="flex mt-3 lg:col-6">
                     <i class="pi pi-list feature-icon"></i>
                     <span>Упрощённое формирование заявок по консолидированным закупках с несколькими участниками;</span>
                 </div>
-                <div class="col-6">
+                <div class="flex mt-3 lg:col-6">
                     <i class="pi pi-chart-bar feature-icon"></i>
                     <span>Универсальные выгрузки данных о закупках и движении препаратов для маркетингового анализа;</span>
                 </div>
-                <div class="col-6">
+            </div>
+            <div class="flex align-items-stretch flex-column lg:flex-row">
+                <div class="flex mt-3 lg:col-6">
                     <i class="pi pi-dollar feature-icon"></i>
                     <span>Контролировать закупочные цены и формировать оптимальный пул поставщиков;</span>
                 </div>
-                <div class="col-6">
+                <div class="flex mt-3 lg:col-6">
                     <i class="pi pi-file feature-icon"></i>
                     <span>Оформлять заказы с использованием электронного документооборота.</span>
                 </div>
