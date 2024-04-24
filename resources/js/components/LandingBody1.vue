e3b5da962700f149bbba607829b9406d367e1b66
Makhnev_SA <ragnoboy123@gmail.com>
2024-04-24 14:01:42 +0200

    fix header

--- resources/js/components/LandingBody1.vue
+++ resources/js/components/LandingBody1.vue
@@ -1,14 +1,14 @@
 <template>
-    <section class="landing-block">
+    <section class="landing-block flex align-items-stretch flex-column lg:flex-row">
         <div class="content-container">
             <h1 class="title">Flow<br><span style="margin-left:5rem;">Expert</span></h1>
             <p class="subtitle">
-                Интеллектуальная система управления заказами лекарственных препаратов и
-                расходных материалов для медицины
-            </p>
+                    Интеллектуальная система управления заказами лекарственных препаратов и
+                    расходных материалов для медицины
+                </p>
             <ConsultationButton/>
         </div>
-        <div class="image-container">
+        <div class="flex image-container">
             <img src="../../pics/landing_image1.webp" alt="Doctor"/>
         </div>
     </section>
@@ -27,24 +27,6 @@ export default {
 
 <style scoped>
 
-@media (min-width: 768px) {
-    .landing-block {
-        border-radius: 7.5px;
-    }
-}
-
-@media (min-width: 1200px) {
-    .landing-block {
-        border-radius: 10px;
-    }
-}
-
-@media (max-width: 1200px) {
-    .image-container img {
-        display: none;
-    }
-}
-
 .title {
     font-size: 5.5rem;
 }
@@ -52,10 +34,15 @@ export default {
 .subtitle {
     font-size: 1rem;
     margin-bottom: 2rem;
-}
+}.image-container{
+     margin-left: 10%;
+     margin-right: 10%;
+ }
 
 .image-container img {
+    width: auto;
     height: auto;
-    margin-right: 10%;
+    max-width: 110%;
+    max-height: 100%;
 }
 </style>
