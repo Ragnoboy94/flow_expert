6afbbbdc80eb357200602d1b7e56f874d4fae07b
Makhnev_SA <ragnoboy123@gmail.com>
2024-05-07 15:18:50 +0200

    admin

--- app/Http/Controllers/Admin/UserCrudController.php
+++ app/Http/Controllers/Admin/UserCrudController.php
@@ -39,7 +39,7 @@ class UserCrudController extends CrudController
      */
     protected function setupListOperation()
     {
-        $this->crud->addColumn(['name' => 'name', 'type' => 'text', 'label' => 'Name']);
+        $this->crud->addColumn(['name' => 'name', 'type' => 'text', 'label' => 'ФИО']);
         $this->crud->addColumn(['name' => 'email', 'type' => 'email', 'label' => 'Email']);
         $this->crud->addColumn(['name' => 'phone', 'type' => 'text', 'label' => 'Телефон']);
         $this->crud->addColumn(['name' => 'company', 'type' => 'text', 'label' => 'Компания']);
@@ -53,14 +53,18 @@ class UserCrudController extends CrudController
      */
     protected function setupCreateOperation()
     {
-        $this->crud->addField(['name' => 'name', 'type' => 'text', 'label' => 'Name']);
+        $this->crud->addField(['name' => 'name', 'type' => 'text', 'label' => 'ФИО']);
         $this->crud->addField(['name' => 'email', 'type' => 'email', 'label' => 'Email']);
-        $this->crud->addField(['name' => 'password', 'type' => 'password', 'label' => 'Password']);
+        $this->crud->addField(['name' => 'phone', 'type' => 'text', 'label' => 'Телефон']);
+        $this->crud->addField(['name' => 'password', 'type' => 'password', 'label' => 'Пароль']);
+        $this->crud->addField(['name' => 'company', 'type' => 'text', 'label' => 'Компания']);
+        $this->crud->addField(['name' => 'position', 'type' => 'text', 'label' => 'Должность']);
         $this->crud->addField([
             'name' => 'inn',
             'type' => 'text',
             'label' => 'ИНН'
         ]);
+        $this->crud->addField(['name' => 'kpp', 'type' => 'text', 'label' => 'КПП']);
 
         $this->crud->addField([
             'label' => "Category",
@@ -70,10 +74,10 @@ class UserCrudController extends CrudController
             'model' => "App\Models\Category",
             'attribute' => 'name',
         ]);
-        $this->crud->addField(['name' => 'phone', 'type' => 'text', 'label' => 'Телефон']);
-        $this->crud->addField(['name' => 'position', 'type' => 'text', 'label' => 'Должность']);
-        $this->crud->addField(['name' => 'company', 'type' => 'text', 'label' => 'Компания']);
-        $this->crud->addField(['name' => 'kpp', 'type' => 'text', 'label' => 'КПП']);
+
+
+
+
     }
 
     /**
