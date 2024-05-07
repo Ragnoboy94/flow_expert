799251bcb9d7f5a175b26278ad73d51539db4d54
Makhnev_SA <ragnoboy123@gmail.com>
2024-05-07 15:17:01 +0200

    admin

--- config/backpack/theme-coreuiv2.php
+++ config/backpack/theme-coreuiv2.php
@@ -25,7 +25,7 @@ return [
     // These can help make the admin panel look similar to your project's design.
     'classes' => [
 
-        'header' => 'app-header bg-light border-0 navbar',
+        'header' => 'app-header bg-dark border-0 navbar ',
         // For background colors use: bg-dark, bg-primary, bg-secondary, bg-danger, bg-warning, bg-success, bg-info, bg-blue, bg-light-blue, bg-indigo, bg-purple, bg-pink, bg-red, bg-orange, bg-yellow, bg-green, bg-teal, bg-cyan, bg-white
         // For links to be visible on different background colors use: "navbar-dark", "navbar-light", "navbar-color"
 
--- config/backpack/ui.php
+++ config/backpack/ui.php
@@ -73,10 +73,10 @@ return [
     // ------
 
     // Developer or company name. Shown in footer.
-    'developer_name' => 'Cristian Tabacitu',
+    'developer_name' => 'Makhnev Sergei',
 
     // Developer website. Link in footer. Type false if you want to hide it.
-    'developer_link' => 'http://tabacitu.ro',
+    'developer_link' => 'https://github.com/Ragnoboy94/',
 
     // Show powered by Laravel Backpack in the footer? true/false
     'show_powered_by' => false,
