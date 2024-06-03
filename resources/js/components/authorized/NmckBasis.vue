66968968e711e938a1089ce7fe9e5c2621ac5598
Makhnev_SA <ragnoboy123@gmail.com>
2024-06-03 16:04:21 +0200

    nmck basis

--- resources/js/components/authorized/NmckBasis.vue
+++ resources/js/components/authorized/NmckBasis.vue
@@ -5,11 +5,14 @@
             <div class="title-section">
                 <h3>Формирование обоснование НМЦК</h3>
                 <p>
-                    Для того, чтобы перейти к скачиванию обоснования НМЦК необходимо загрузить потребность и коммерческие предложения.
+                    Для того, чтобы перейти к скачиванию обоснования НМЦК необходимо загрузить потребность и
+                    коммерческие предложения.
                 </p>
                 <p v-if="!selectedFile" class="text-green-600">Выберите предложение</p>
             </div>
-            <DataTable v-model:selection="selectedFile" :value="files" selectionMode="single" dataKey="id" :metaKeySelection="false" @rowSelect="onRowSelect" @rowUnselect="onRowUnselect" table-style="border-color: green">
+            <DataTable v-model:selection="selectedFile" :value="files" selectionMode="single" dataKey="id"
+                       :metaKeySelection="false" @rowSelect="onRowSelect" @rowUnselect="onRowUnselect"
+                       table-style="border-color: green">
                 <Column field="filename" header="Имя файла"></Column>
                 <Column field="created_at" header="Дата загрузки">
                     <template #body="{ data }">
@@ -22,11 +25,49 @@
                           inputId="openSource"/>
                 <label for="openSource"> Использовать цены из открытых источников</label>
             </div>
-            <Button label="Подготовить файл для скачивания" class="consultation-button" @click="prepareFile" :disabled="!selectedFile"/>
+            <div class="flex flex-column lg:flex-row lg:flex-wrap mt-3">
+                <Fieldset class="border-round-3xl  lg:col-8 col-12" legend="Метод средневзвешенной цены">
+                    <p class="m-0">
+                        <div class="flex-1 lg:mr-2 mb-3">
+                            <DataTable stripedRows :value="monthlyData" editMode="cell" @cell-edit-complete="onCellEditComplete">
+                                <Column field="month" header="Месяц" :editable="false"></Column>
+                                <Column field="price" header="Цена"  style="width: 33%">
+                                    <template #editor="{ data, field }">
+                                        <InputNumber v-model="data[field]"/>
+                                    </template>
+                                </Column>
+                                <Column field="quantity" header="Количество"  style="width: 33%">
+                                    <template #editor="{ data, field }">
+                                        <InputNumber v-model="data[field]"/>
+                                    </template>
+                                </Column>
+                            </DataTable>
+                        </div>
+                    </p>
+                </Fieldset>
+                <Fieldset class="border-round-3xl  lg:col-4 col-12" legend="Метод референтных цен">
+                    <p class="m-0">
+                        <div class="flex-1 lg:ml-2 mb-3">
+                            <DataTable stripedRows :value="periodicData" editMode="cell" @cell-edit-complete="onCellEditComplete">
+                                <Column field="period" header="Период" :editable="false" style="width: 50%"></Column>
+                                <Column field="quantity" header="Количество" style="width: 50%">
+                                    <template #editor="{ data, field }">
+                                        <InputNumber v-model="data[field]"/>
+                                    </template>
+                                </Column>
+                            </DataTable>
+                        </div>
+                    </p>
+                </Fieldset>
+            </div>
+            <Button label="Подготовить файл для скачивания" class="consultation-button" @click="prepareFile"
+                    :disabled="!selectedFile"/>
+
         </div>
     </section>
     <Footer></Footer>
 </template>
+
 
 <script>
 import Header from "./../Header.vue";
@@ -34,15 +75,37 @@ import Footer from "./../Footer.vue";
 import DataTable from "primevue/datatable";
 import Checkbox from 'primevue/checkbox';
 import Column from 'primevue/column';
+import InputNumber from "primevue/inputnumber";
 import {mapActions, mapState} from "vuex";
-
+import Fieldset from "primevue/fieldset";
 
 export default {
-    components: {Header, Footer, DataTable, Checkbox, Column},
+    components: {Header, Footer, DataTable, Checkbox, Column, InputNumber, Fieldset},
     data() {
+        const currentYear = new Date().getFullYear();
         return {
             selectedFile: null,
-            openSource: false
+            openSource: false,
+            monthlyData: [
+                {month: 'Январь', price: 0, quantity: 0},
+                {month: 'Февраль', price: 0, quantity: 0},
+                {month: 'Март', price: 0, quantity: 0},
+                {month: 'Апрель', price: 0, quantity: 0},
+                {month: 'Май', price: 0, quantity: 0},
+                {month: 'Июнь', price: 0, quantity: 0},
+                {month: 'Июль', price: 0, quantity: 0},
+                {month: 'Август', price: 0, quantity: 0},
+                {month: 'Сентябрь', price: 0, quantity: 0},
+                {month: 'Октябрь', price: 0, quantity: 0},
+                {month: 'Ноябрь', price: 0, quantity: 0},
+                {month: 'Декабрь', price: 0, quantity: 0}
+            ],
+            periodicData: [
+                { period: `01.01.${currentYear}`, quantity: 0 },
+                { period: `01.04.${currentYear}`, quantity: 0 },
+                { period: `01.07.${currentYear}`, quantity: 0 },
+                { period: `01.10.${currentYear}`, quantity: 0 }
+            ]
         };
     },
     methods: {
@@ -70,6 +133,10 @@ export default {
             } catch (error) {
                 console.error('Ошибка при подготовке файла:', error);
             }
+        },
+        onCellEditComplete(event) {
+            let { data, newValue, field } = event;
+            data[field] = newValue;
         }
     },
     computed: {
